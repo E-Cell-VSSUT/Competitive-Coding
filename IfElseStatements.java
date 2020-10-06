93d233e1c3979ed64fbc02729ac3ff915ac2db37
pratikpanda-debug <69814333+pratikpanda-debug@users.noreply.github.com>
2020-10-06 19:21:30 +0530

    ifelse statements in java
    problem to find whether the age is eligible to vote or not using ifelse staements in java.

---
+++ IfElseStatements.java
@@ -0,0 +1,17 @@
+package ConditionalStatements;
+
+public class IfElseStatements {
+
+	public static void main(String[] args) {
+		int age=19;    
+		if(age>=18) {
+			System.out.println("you can vote now.");
+		}else {
+			System.out.println("you can not vote now.");
+		}
+		
+		
+	}
+	
+}
+	
